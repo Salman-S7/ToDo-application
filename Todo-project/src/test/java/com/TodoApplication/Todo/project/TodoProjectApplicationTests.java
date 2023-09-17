b3ae4706057d8a0af327d9ea91dcffc3a2e1d4f1
Salman-S7 <sammyzed1505@gmail.com>
2023-09-17 20:04:18 +0530

    configured project

---
+++ Todo-project/src/test/java/com/TodoApplication/Todo/project/TodoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TodoApplication.Todo.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
